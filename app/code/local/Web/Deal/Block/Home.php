c4814286f60fd594be707e29df475cf358b18b4b
Mohamed Ahmed <mo.meabed@gmail.com>
2012-11-13 21:27:53 +0400

    Changes

--- app/code/local/Web/Deal/Block/Home.php
+++ app/code/local/Web/Deal/Block/Home.php
@@ -8,14 +8,15 @@ class Web_Deal_Block_Home extends Mage_Core_Block_Template
     {
         //echo "X";
         //echo $this->_products->count();
-
     }
     public function getProducts()
     {
+        $catCollection = Mage::helper('deal')->getActiveCategories(null);
+        $catIds = $catCollection->getAllIds();
         $this->_products = Mage::getModel('catalog/product')->getCollection()
+            ->joinField('category_id','catalog/category_product','category_id','product_id=entity_id',null,'left')
             ->addAttributeToSelect('*')
-            //->addAttributeToFilter('main_deal',array('eq'=>'1'))
-
+        //->addAttributeToFilter('main_deal',array('eq'=>'1'))
             ->addAttributeToFilter('start_date',array('to'=>Mage::getModel('core/date')->date('Y-m-d H:i:s')))
             ->addAttributeToFilter('end_date',array('from'=>Mage::getModel('core/date')->date('Y-m-d H:i:s')))
             ->addAttributeToSort('main_deal','DESC')
@@ -23,6 +24,7 @@ class Web_Deal_Block_Home extends Mage_Core_Block_Template
             ->addAttributeToSort('created_at','DESC')
             ->addAttributeToFilter('status', array('eq' => Mage_Catalog_Model_Product_Status::STATUS_ENABLED))
             ->addAttributeToFilter('visibility', array('neq' => Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE))
+            ->addAttributeToFilter('category_id', array('in' => $catIds))
             ->load();
         $skus = array();
         foreach($this->_products as $product)
--- app/code/local/Web/Deal/Helper/Data.php
+++ app/code/local/Web/Deal/Helper/Data.php
@@ -2,34 +2,36 @@
 class Web_Deal_Helper_Data extends Mage_Core_Helper_Abstract
 {
     public static function getFullProductUrl(Mage_Catalog_Model_Product $product,
-                                      Mage_Catalog_Model_Category $category = null,
-                                      $mustBeIncludedInNavigation = true)
+                                             Mage_Catalog_Model_Category $category = null,
+                                             $mustBeIncludedInNavigation = true)
     {
         $currentCategory = Mage::registry('current_category');
         $catIds = $product->getCategoryIds();
         $url = Mage::getModel('core/url_rewrite')->getCollection()
-            ->addFilter('is_system','1')
-            ->addFilter('product_id',$product->getId())
-            ->addFilter('store_id',Mage::app()->getStore()->getId())
-            ->addFieldToFilter('category_id',array('gt'=>'0'));
+            ->addFilter('is_system', '1')
+            ->addFilter('product_id', $product->getId())
+            ->addFilter('store_id', Mage::app()->getStore()->getId())
+            ->addFieldToFilter('category_id', array('gt' => '0'));
 
-        if($currentCategory && in_array($currentCategory->getId(),$catIds) ){
+        if ($currentCategory && in_array($currentCategory->getId(), $catIds)) {
             $catId = $currentCategory->getId();
-            $url->addFilter('category_id',$catId);
+            $url->addFilter('category_id', $catId);
         }
-        $rs = $url->setOrder('url_rewrite_id','DESC')->getFirstItem();
-        if(!$rs->getRequestPath()){
+        $rs = $url->setOrder('url_rewrite_id', 'DESC')->getFirstItem();
+        if (!$rs->getRequestPath()) {
             return $product->getUrlPath();
         }
-        return Mage::getBaseUrl() .$rs->getRequestPath();
+        return Mage::getBaseUrl() . $rs->getRequestPath();
 
     }
+
     public function getProductCategoryName(Mage_Catalog_Model_Product $product)
     {
         $productCategories = $product->getCategoryIds();
 
-        if(!count($productCategories))
-        {return;}
+        if (!count($productCategories)) {
+            return;
+        }
         $tmpCategory = Mage::getModel('catalog/category')->load($productCategories[0]);
         //echo $tmpCategory->getId();
         return $tmpCategory->getName();
@@ -52,38 +54,50 @@ class Web_Deal_Helper_Data extends Mage_Core_Helper_Abstract
     {
         return Mage::app()->getStore()->getCurrentCurrencyCode() . ' ' . Mage::helper('core')->currency($price, false);
     }
-   public function getDateDiff($date1,$date2)
-   {
-       $epoch_1 = strtotime($date1);
-       $epoch_2 = strtotime($date2);
 
-       $diff_seconds  = $epoch_1 - $epoch_2;
-       if($diff_seconds <= 0)
-       {
-           return 0 ;
-       }
+    public function getDateDiff($date1, $date2)
+    {
+        $epoch_1 = strtotime($date1);
+        $epoch_2 = strtotime($date2);
 
-           $s = ' Days';
-           $diff  = floor($diff_seconds/86400); // days
-           if($diff == 0)
-           {
-               $s = ' Hours';
-               $diff = floor($diff_seconds/3600); // hours
-               if($diff == 0)
-               {
-                   $s = ' Minutes';
-                   $diff  = floor($diff_seconds/60); // minutes
-                   if($diff == 0)
-                   {
-                       $s = ' Seconds';
-                       $diff = $diff_seconds;
-                   }
-               }
-           }
+        $diff_seconds = $epoch_1 - $epoch_2;
+        if ($diff_seconds <= 0) {
+            return 0;
+        }
 
-       return $diff.$s;
+        $s = ' Days';
+        $diff = floor($diff_seconds / 86400); // days
+        if ($diff == 0) {
+            $s = ' Hours';
+            $diff = floor($diff_seconds / 3600); // hours
+            if ($diff == 0) {
+                $s = ' Minutes';
+                $diff = floor($diff_seconds / 60); // minutes
+                if ($diff == 0) {
+                    $s = ' Seconds';
+                    $diff = $diff_seconds;
+                }
+            }
+        }
 
-   }
+        return $diff . $s;
+
+    }
+    public function getActiveCategories($mainCat = null)
+    {
+        if($mainCat == null)
+        {
+            $catCookie = Mage::getModel('core/cookie')->get('main_cat');
+            $mainCat = empty($catCookie) ? (int)Mage::getStoreConfig('deal/config/main_cat') : Mage::getModel('core/cookie')->get('main_cat');
+        }
+        $cats = Mage::getModel('catalog/category')->getCollection()
+            ->addAttributeToSelect('name')
+            ->addFilter('parent_id', $mainCat)
+            ->addFilter('level', '3')
+            ->addAttributeToFilter('include_in_menu',array('eq'=>1))
+            ->addAttributeToFilter('is_active',array('eq'=>1));
+        return $cats;
+    }
 
 }
 	 
