58ed2f940439d2a5f810048f3fc244af118ffe86
Mohamed Ahmed <mo.meabed@gmail.com>
2013-01-24 13:59:23 +0400

    Migration test

---
+++ test99/moveCustomers.php
@@ -0,0 +1,15 @@
+<?php
+$_magePathNew = '../app/Mage.php';
+
+$_magePath = '/home/dealgobb/dealgobbler.com/html/Mage.php';
+
+include_once($_magePath);
+
+//$dbh = new PDO('mysql:host=localhost;dbname=dealgobb_groupclone332', 'dealgobb_tariq', 'PimplyForeGeneraApathy');
+
+Mage::app();
+
+$customers = Mage::getModel('customer/customer')->getCollection();
+
+
+echo $customers->getSize();
