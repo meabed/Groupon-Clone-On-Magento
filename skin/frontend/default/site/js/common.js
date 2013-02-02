fb3d6acd63152f1f0952dfa5f0f960a31de9a4d6
Mohamed Ahmed <mo.meabed@gmail.com>
2013-02-02 14:16:11 +0400

    Fix

--- skin/frontend/default/site/js/common.js
+++ skin/frontend/default/site/js/common.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function() {
   jQuery('img.lazy').jail({
     event: 'load+scroll',
     placeholder : "/skin/frontend/default/site/images/loader.gif"
