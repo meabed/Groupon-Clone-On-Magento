d2b957ba82a74b725ad21b28b6610f8854440951
Mohamed Ahmed <mo.meabed@gmail.com>
2013-02-03 21:05:39 +0400

    Fix

--- app/code/local/Web/Deal/Model/Observer.php
+++ app/code/local/Web/Deal/Model/Observer.php
@@ -3,6 +3,12 @@ class Web_Deal_Model_Observer
 {
     public function disableAddToCart( Varien_Event_Observer $observer )
     {
-        echo "X";exit();
+        $cart = $observer->getEvent()->getCart();
+        $items = $cart->getItems();
+        foreach ($items as $item)
+        {
+            echo $item->getEndDate().'--';
+        }
+        exit();
     }
 }
