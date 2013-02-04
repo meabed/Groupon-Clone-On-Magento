264043ea919a5566008e657563cf8dc6fe8ea192
Mohamed Ahmed <mo.meabed@gmail.com>
2013-02-04 22:55:54 +0400

    Fix

--- app/code/local/Web/Voucher/Block/History.php
+++ app/code/local/Web/Voucher/Block/History.php
@@ -15,7 +15,7 @@ class Web_Voucher_Block_History extends Mage_Core_Block_Template
                 ->joinLeft(array('orders' => $ordersTable), 'main_table.order_id=orders.entity_id', array('orders.customer_firstname', 'orders.customer_lastname', 'order_status' => 'orders.status'))
                 ->where('orders.customer_id = ' . Mage::getSingleton('customer/session')->getCustomer()->getId())
                 ->order(array('main_table.created_at desc','main_table.order_id desc'));
-        echo $vouchers->getSelect()->__toString();
+        //echo $vouchers->getSelect()->__toString();
         $this->setVouchers($vouchers);
         
         Mage::app()->getFrontController()->getAction()->getLayout()->getBlock('root')->setHeaderTitle(Mage::helper('voucher')->__('My Vouchers'));
