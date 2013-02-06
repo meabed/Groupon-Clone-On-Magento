037b7bc06bab806fd2b4dc8d8fa0345a04c95cb4
Mohamed Ahmed <mo.meabed@gmail.com>
2013-02-06 23:13:33 +0400

    Fix

--- app/code/local/Web/Voucher/Block/Recent.php
+++ app/code/local/Web/Voucher/Block/Recent.php
@@ -14,6 +14,7 @@ class Web_Voucher_Block_Recent extends Mage_Core_Block_Template
         $vouchers->getSelect()
                 ->joinLeft(array('orders' => $ordersTable), 'main_table.order_id=orders.entity_id', array('orders.customer_firstname', 'orders.customer_lastname', 'order_status'=>'orders.status'))
                 ->where('orders.customer_id = ' . Mage::getSingleton('customer/session')->getCustomer()->getId())
+                ->where('main_table.customer_id = ' . Mage::getSingleton('customer/session')->getCustomer()->getId())
                 ->order(array('main_table.created_at desc','main_table.order_id desc'))
                 ->limit('10');
         $this->setVouchers($vouchers);
