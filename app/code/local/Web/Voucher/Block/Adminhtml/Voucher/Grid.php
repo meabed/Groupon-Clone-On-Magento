c854d24a2e3879e892edddd5f1f08811629b531d
Mohamed Ahmed <mo.meabed@gmail.com>
2013-02-11 21:46:39 +0400

    Fix

--- app/code/local/Web/Voucher/Block/Adminhtml/Voucher/Grid.php
+++ app/code/local/Web/Voucher/Block/Adminhtml/Voucher/Grid.php
@@ -22,7 +22,7 @@ class Web_Voucher_Block_Adminhtml_Voucher_Grid extends Mage_Adminhtml_Block_Widg
         $collection->getSelect()
             ->joinLeft(array('orders' => $ordersTable), 'main_table.order_id=orders.entity_id', array('orders.customer_firstname', 'orders.customer_lastname', 'order_status' => 'orders.status'));
         $collection->getSelect()
-            ->joinLeft(array('address' => $addressTable), 'orders.order_id=address.billing_address_id', array('address.street', 'address.city', 'address.phone'));
+            ->joinLeft(array('address' => $addressTable), 'orders.billing_address_id=address.entity_id', array('address.street', 'address.city', 'address.phone'));
         $collection->getSelect()
             ->joinLeft(array('product' => $productTable), 'main_table.product_id = product.entity_id', array('name'));
         $this->setCollection($collection);
