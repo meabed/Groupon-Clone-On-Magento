fe14d1d9397508a31f94368b7c6570740fa29880
Mohamed Ahmed <mo.meabed@gmail.com>
2013-03-09 14:32:15 +0400

    Fix

--- app/code/local/Web/Voucher/controllers/Adminhtml/VoucherController.php
+++ app/code/local/Web/Voucher/controllers/Adminhtml/VoucherController.php
@@ -134,7 +134,7 @@ class Web_Voucher_Adminhtml_VoucherController extends Mage_Adminhtml_Controller_
                 $cmd = Mage::getBaseDir('lib').DS.'wkhtmltopdf '.$string.' '.$fname;
                 $r = exec($cmd);
                 $this->_getSession()->addSuccess(
-                    $this->__('Total of %d vouchers(s) generated <a href=">'.Mage::getUrl(). '/media/vouchers/' .md5($string).'.pdf">Here</a>' , count($voucherIds))
+                    $this->__('Total of %d vouchers(s) generated <a href="'.Mage::getBaseUrl(). '/media/vouchers/' .md5($string).'.pdf">Here</a>' , count($voucherIds))
                 );
             } catch (Exception $e) {
                 $this->_getSession()->addError($e->getMessage());
