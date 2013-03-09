8cfa4d0e5bfaaf394ee8ee3ee92842dd4ade2473
Mohamed Ahmed <mo.meabed@gmail.com>
2013-03-09 14:17:55 +0400

    Fix

--- app/code/local/Web/Voucher/controllers/Adminhtml/VoucherController.php
+++ app/code/local/Web/Voucher/controllers/Adminhtml/VoucherController.php
@@ -115,18 +115,12 @@ class Web_Voucher_Adminhtml_VoucherController extends Mage_Adminhtml_Controller_
                 foreach ($voucherIds as $voucherId) {
                     $voucher = $voucherModel->load($voucherId);
                     $auth = strtoupper(md5(strtoupper($voucher->getDealVoucherCode()).'213@#$%^$DFSfwer@!#'.$voucher->getOrderId()));
-                    //$url = Mage::getUrl('voucher/view/downloadadmin',array('code'=>$voucher->getDealVoucherCode(),'auth'=> $auth));
-                    //$client = new Zend_Http_Client();
-                    //$response = $client->setUri($url)->request();
-                    $rs = Mage::helper('voucher')->downloadAdmin($voucher->getDealVoucherCode(),$auth);
-                    if($rs){
-                        $urls[] = $rs;
-                    }
-                    //$urlx = file_get_contents($url);
+                    $url = Mage::getUrl('voucher/view/downloadadmin',array('code'=>$voucher->getDealVoucherCode(),'auth'=> $auth));
+                    $urlx = file_get_contents(trim(urlencode($url)));
+                    $urls[] = $urlx;
                    // Mage::getModel('voucher/observer')->_sendVoucherEmail($voucher);
                     //$voucher->setIsSent($voucher->getIsSent()+1)->save();
                 }
-                print_r($urls);exit;
                 $string = join(' ',array_unique($urls));
                 $fname = Mage::getBaseDir('media').DS.'vouchers'.DS.md5($string).'.pdf';
                 $cmd = Mage::getBaseDir('lib').DS.'wkhtmltopdf '.$string.' '.$fname;
