4e6662eb8ba940dc780599363af73e3bbb442dcd
Mohamed Ahmed <mo.meabed@gmail.com>
2013-07-15 01:19:43 +0400

    Latest Update ( Bootstrap + DB file )

--- app/code/local/Web/Migs/Block/Migs.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class Web_Migs_Block_Migs extends Mage_Payment_Block_Form_Cc
-{
-
-}
--- app/code/local/Web/Migs/Block/Success.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Web_Migs_Block_Success extends Mage_Checkout_Block_Onepage_Success
-{
-    
-}
