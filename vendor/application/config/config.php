e0ac88977c766d91d09af3db2b4df547a3eb4210
Mohamed Ahmed <mo.meabed@gmail.com>
2013-07-25 01:23:04 +0400

    Vendor Module

--- vendor/application/config/config.php
+++ vendor/application/config/config.php
@@ -14,7 +14,16 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://mage.me/vendor/';
+function url(){
+    if(isset($_SERVER['HTTPS'])){
+        $protocol = ($_SERVER['HTTPS'] && $_SERVER['HTTPS'] != "off") ? "https" : "http";
+    }
+    else{
+        $protocol = 'http';
+    }
+    return $protocol . "://" . $_SERVER['HTTP_HOST'] ;
+}
+$config['base_url']	= url().'/vendor/';
 
 /*
 |--------------------------------------------------------------------------
